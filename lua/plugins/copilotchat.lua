46e104339fedd991aa72310687d65ecb73e8bd1c
Sargis <sargis@eluve.com>
2025-08-12 19:30:32 -0700

    add copilot chat configs

---
+++ lua/plugins/copilotchat.lua
@@ -0,0 +1,77 @@
+return {
+  "CopilotC-Nvim/CopilotChat.nvim",
+  branch = "main",
+  cmd = "CopilotChat",
+  opts = function()
+    local user = vim.env.USER or "User"
+    return {
+
+      model = "gpt-4.1", -- AI model to use
+      temperature = 0.1, -- Lower = focused, higher = creative
+      window = {
+        layout = "vertical", -- 'vertical', 'horizontal', 'float'
+        width = 0.5, -- 50% of screen width
+      },
+      user = user:sub(1, 1):upper() .. user:sub(2),
+
+      auto_insert_mode = true,
+      question_header = "  " .. user .. " ",
+      answer_header = "  Copilot ",
+    }
+  end,
+  keys = {
+    { "<c-s>", "<CR>", ft = "copilot-chat", desc = "Submit Prompt", remap = true },
+    { "<leader>a", "", desc = "+ai", mode = { "n", "v" } },
+    {
+      "<leader>aa",
+      function()
+        return require("CopilotChat").toggle()
+      end,
+      desc = "Toggle (CopilotChat)",
+      mode = { "n", "v" },
+    },
+    {
+      "<leader>ax",
+      function()
+        return require("CopilotChat").reset()
+      end,
+      desc = "Clear (CopilotChat)",
+      mode = { "n", "v" },
+    },
+    {
+      "<leader>aq",
+      function()
+        vim.ui.input({
+          prompt = "Quick Chat: ",
+        }, function(input)
+          if input ~= "" then
+            require("CopilotChat").ask(input)
+          end
+        end)
+      end,
+      desc = "Quick Chat (CopilotChat)",
+      mode = { "n", "v" },
+    },
+    {
+      "<leader>ap",
+      function()
+        require("CopilotChat").select_prompt()
+      end,
+      desc = "Prompt Actions (CopilotChat)",
+      mode = { "n", "v" },
+    },
+  },
+  config = function(_, opts)
+    local chat = require("CopilotChat")
+
+    vim.api.nvim_create_autocmd("BufEnter", {
+      pattern = "copilot-chat",
+      callback = function()
+        vim.opt_local.relativenumber = false
+        vim.opt_local.number = false
+      end,
+    })
+
+    chat.setup(opts)
+  end,
+}
